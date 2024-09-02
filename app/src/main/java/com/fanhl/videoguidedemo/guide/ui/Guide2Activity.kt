a8b9290e17f5c9c219ad82faabf09bf18c2d3fca
dyguests <dyguests@qq.com>
2024-09-02 16:25:03 +0800

    guide Activity use xml

---
+++ app/src/main/java/com/fanhl/videoguidedemo/guide/ui/Guide2Activity.kt
@@ -0,0 +1,31 @@
+package com.fanhl.videoguidedemo.guide.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.fanhl.videoguidedemo.R
+import com.fanhl.videoguidedemo.databinding.ActivityGuideBinding
+
+class Guide2Activity : AppCompatActivity() {
+    lateinit var binding: ActivityGuideBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityGuideBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val exitable = intent.getBooleanExtra(KEY_EXITABLE, false)
+    }
+
+    companion object {
+        private const val TAG = "GuideActivity"
+
+        const val KEY_EXITABLE = "KEY_EXITABLE"
+    }
+}
