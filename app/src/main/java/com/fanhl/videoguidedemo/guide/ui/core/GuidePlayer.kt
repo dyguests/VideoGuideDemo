f55d9bf136eb73e6094e544947b6c646e3b9a84c
dyguests <dyguests@qq.com>
2024-09-02 17:38:49 +0800

    顺序播放视频

--- app/src/main/java/com/fanhl/videoguidedemo/guide/ui/core/GuidePlayer.kt
+++ app/src/main/java/com/fanhl/videoguidedemo/guide/ui/core/GuidePlayer.kt
@@ -8,6 +8,7 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.lifecycle.lifecycleScope
 import com.fanhl.videoguidedemo.guide.ui.entity.IClip
 import com.fanhl.videoguidedemo.guide.ui.entity.VideoClip
+import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 
@@ -38,13 +39,29 @@ class GuidePlayer(
     }
 
     private suspend fun playVideo(clip: VideoClip) {
-        check(clip.sourceType == VideoClip.SOURCE_RAW) { "sourceType is not SOURCE_RAW" }// todo
+        check(clip.sourceType == VideoClip.SOURCE_RAW) { "sourceType is not SOURCE_RAW" }
         val rawSource = clip.rawSource ?: throw IllegalArgumentException("rawSource is null")
         val videoUri = Uri.parse("android.resource://" + activity.packageName + "/" + rawSource)
         videoView.setVideoURI(videoUri)
         interactionLayer.removeAllViews()
+
+        // 创建一个 CompletableDeferred 用于等待视频播放完成
+        val playbackCompleted = CompletableDeferred<Unit>()
+
+        // 设置播放完成监听器
+        videoView.setOnCompletionListener {
+            // 移除监听器，防止内存泄漏
+            videoView.setOnCompletionListener(null)
+            // 当视频播放完成时，complete playbackCompleted
+            playbackCompleted.complete(Unit)
+        }
+
         videoView.start()
+
+        // 等待视频播放完成
+        playbackCompleted.await()
     }
+
 
     companion object {
         private const val TAG = "GuidePlayer"
