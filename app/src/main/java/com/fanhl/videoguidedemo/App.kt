cef8cf6e39c1c89093ab551f1e50ab124b9e019a
dyguests <dyguests@qq.com>
2024-09-02 15:31:01 +0800

    app context.

--- app/src/main/java/com/fanhl/videoguidedemo/App.kt
+++ app/src/main/java/com/fanhl/videoguidedemo/App.kt
@@ -1,12 +1,11 @@
 package com.fanhl.videoguidedemo
 
 import android.app.Application
-import android.content.Context
 import com.fanhl.videoguidedemo.util.ContextUtil
 
 class App : Application() {
-    override fun attachBaseContext(base: Context?) {
-        super.attachBaseContext(base)
-        ContextUtil.init(base)
+    override fun onCreate() {
+        super.onCreate()
+        ContextUtil.init(this)
     }
 }
