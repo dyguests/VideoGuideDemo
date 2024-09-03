fcb97ea64dce6037a8beff327cd3b34bc2554784
dyguests <dyguests@qq.com>
2024-09-03 13:41:48 +0800

    navi

--- app/src/main/java/com/fanhl/videoguidedemo/util/NaviEx.kt
+++ app/src/main/java/com/fanhl/videoguidedemo/util/NaviEx.kt
@@ -1,5 +1,6 @@
 package com.fanhl.videoguidedemo.util
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import androidx.activity.ComponentActivity
@@ -7,4 +8,10 @@ import kotlin.reflect.KClass
 
 fun Context.navi(kClass: KClass<out ComponentActivity>, intentBlock: (Intent.() -> Unit)? = null) {
     this.startActivity(Intent(this, kClass.java).apply { intentBlock?.invoke(this) })
+}
+fun Context.naviForResult(kClass: KClass<out ComponentActivity>, requestCode: Int, intentBlock: (Intent.() -> Unit)? = null) =
+    (this as? Activity)?.naviForResult(kClass, requestCode, intentBlock) ?: throw IllegalStateException("Context is not Activity")
+
+fun Activity.naviForResult(kClass: KClass<out ComponentActivity>, requestCode: Int, intentBlock: (Intent.() -> Unit)? = null) {
+    this.startActivityForResult(Intent(this, kClass.java).apply { intentBlock?.invoke(this) }, requestCode)
 }
